c99dc3612d691b6c599641b6d6fc25759d477b5c
João Duarte <2181984@my.ipleiria.pt>
2020-11-17 17:47:01 +0000

    commit

--- OsteoClinic/app/src/main/java/amsi/dei/estg/ipleiria/osteoclinic/vistas/MenuMainActivity.java
+++ OsteoClinic/app/src/main/java/amsi/dei/estg/ipleiria/osteoclinic/vistas/MenuMainActivity.java
@@ -1,18 +1,49 @@
 package amsi.dei.estg.ipleiria.osteoclinic.vistas;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 import amsi.dei.estg.ipleiria.osteoclinic.R;
 
-public class MenuMainActivity extends AppCompatActivity {
+public class MenuMainActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
 
     public static final String EMAIL = "amsi.dei.estg.ipleiria.osteoclinic.vistas.email";
+
+
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_main);
+
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.app_toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView = findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.ndOpen, R.string.ndClose);
+        toggle.syncState();
+        drawer.addDrawerListener(toggle);
+
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
