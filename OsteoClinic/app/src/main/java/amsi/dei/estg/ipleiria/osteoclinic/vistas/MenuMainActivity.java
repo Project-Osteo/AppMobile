bf3fee1024793acbab3e7cceccd519f95cf6dae0
João Duarte <2181984@my.ipleiria.pt>
2020-11-17 15:48:18 +0000

    maneu mani actvity - start

---
+++ OsteoClinic/app/src/main/java/amsi/dei/estg/ipleiria/osteoclinic/vistas/MenuMainActivity.java
@@ -0,0 +1,16 @@
+package amsi.dei.estg.ipleiria.osteoclinic.vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import amsi.dei.estg.ipleiria.osteoclinic.R;
+
+public class MenuMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_main);
+    }
+}
