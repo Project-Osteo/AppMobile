cbbd60dc6dadf36b258bea4fa09ca8116eddbb58
jbrigido <jbrigido_14@hotmail.com>
2020-11-18 21:04:29 +0000

    botao consulta já abre o fragmento lista de consultas

--- OsteoClinic/app/src/main/java/amsi/dei/estg/ipleiria/osteoclinic/vistas/InicialFragment.java
+++ OsteoClinic/app/src/main/java/amsi/dei/estg/ipleiria/osteoclinic/vistas/InicialFragment.java
@@ -3,15 +3,20 @@ package amsi.dei.estg.ipleiria.osteoclinic.vistas;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 import amsi.dei.estg.ipleiria.osteoclinic.R;
 
 
-public class InicialFragment extends Fragment {
+public class InicialFragment extends Fragment implements View.OnClickListener {
+
+    private Button button1, button2, button3;
 
 
     public InicialFragment() {
@@ -27,6 +32,31 @@ public class InicialFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicial, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_inicial, container, false);
+
+        button1 = view.findViewById(R.id.btConsultas);
+
+        button1.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Fragment fragmento = null;
+        switch (view.getId()) {
+            case R.id.btConsultas:
+                fragmento = new ListaConsultasFragment();
+                replaceFragment(fragmento);
+                break;
+        }
+    }
+
+    private void replaceFragment(Fragment fragmento) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.contentFragment, fragmento);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
