38d72c10a54ed0fff849851b15c675bd297b0942
jbrigido <jbrigido_14@hotmail.com>
2020-12-12 17:17:26 +0000

    detalhes treino

--- OsteoClinic/app/src/main/java/amsi/dei/estg/ipleiria/osteoclinic/vistas/DetalhesTreinoActivity.java
+++ OsteoClinic/app/src/main/java/amsi/dei/estg/ipleiria/osteoclinic/vistas/DetalhesTreinoActivity.java
@@ -3,14 +3,27 @@ package amsi.dei.estg.ipleiria.osteoclinic.vistas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import amsi.dei.estg.ipleiria.osteoclinic.R;
 
 public class DetalhesTreinoActivity extends AppCompatActivity {
 
+    private static final String ID_TREINO = "amsi.dei.estg.ipleiria.osteoclinic.vistas.ID";
+
+    private TextView tvTipoTreino, tvDataTreino, tvTerapeutaTreino, tvDescricaoTreino, tvRecomendacoesTreino;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_treino);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        tvTipoTreino = findViewById(R.id.tvTipoTreinoDetalhe);
+        tvDataTreino = findViewById(R.id.tvDataTreinoDetalhe);
+        tvTerapeutaTreino = findViewById(R.id.tvTerapeutaDetalheTreino);
+        tvDescricaoTreino = findViewById(R.id.tvDescricaoTreinoDetalhe);
+        tvRecomendacoesTreino = findViewById(R.id.tvRecomendacoesTreinoDetalhe);
+        
     }
 }
