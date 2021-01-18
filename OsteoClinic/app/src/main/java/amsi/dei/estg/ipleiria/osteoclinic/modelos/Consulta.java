910b1db65f59d4c301758e12923440411835144c
jotaduart <jotaduart@gmail.com>
2021-01-18 22:49:37 +0000

    fixs

--- OsteoClinic/app/src/main/java/amsi/dei/estg/ipleiria/osteoclinic/modelos/Consulta.java
+++ OsteoClinic/app/src/main/java/amsi/dei/estg/ipleiria/osteoclinic/modelos/Consulta.java
@@ -7,17 +7,15 @@ public class Consulta {
     private long id;
     private Date data_consulta;
     private String descricao_consulta;
-    private long paciente_id;
     private String tratamento;
     private String obs_consulta;
     private String recomendacao;
 
-    public Consulta(long id, Date data_consulta, String descricao, long paciente_id,
+    public Consulta(long id, Date data_consulta, String descricao,
                     String tratamento, String obs, String recomendacao) {
         this.id = id;
         this.data_consulta = data_consulta;
         this.descricao_consulta = descricao;
-        this.paciente_id = paciente_id;
         this.tratamento = tratamento;
         this.obs_consulta = obs;
         this.recomendacao = recomendacao;
@@ -35,31 +33,16 @@ public class Consulta {
         return descricao_consulta;
     }
 
-    public void setDescricao_consulta(String descricao_consulta) {
-        this.descricao_consulta = descricao_consulta;
-    }
-
     public String getTratamento() {
         return tratamento;
-    }
-
-    public void setTratamento(String tratamento) {
-        this.tratamento = tratamento;
     }
 
     public String getObs_consulta() {
         return obs_consulta;
     }
 
-    public void setObs_consulta(String obs_consulta) {
-        this.obs_consulta = obs_consulta;
-    }
-
     public String getRecomendacao() {
         return recomendacao;
     }
 
-    public void setRecomendacao(String recomendacao) {
-        this.recomendacao = recomendacao;
-    }
 }
